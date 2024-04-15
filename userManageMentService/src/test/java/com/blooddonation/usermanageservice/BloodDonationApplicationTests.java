ad70661bcc766a4ed7b6f556f500f34987fdee20
Shashank Suresh Kumar <shashank.kumar24@ad.infosys.com>
2024-04-15 11:36:04 +0530

    first commit

---
+++ userManageMentService/src/test/java/com/blooddonation/usermanageservice/BloodDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blooddonation.usermanageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
